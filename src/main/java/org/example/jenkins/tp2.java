7049cb27e1cb219a9bc2aeb217dffa7c07681727
bousselham elabd <bousselham.elabd2000@gmail.com>
2025-12-17 17:12:00 +0100

    first commit second commit

---
+++ src/main/java/org/example/jenkins/tp2.java
@@ -0,0 +1,10 @@
+package org.example.jenkins;
+
+public class tp2 {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 20;
+        System.out.println("La somme = " + (A + B));
+    }
+
+}
